8c4c60ef63b89d67bbfadc0c84c06f0491c21e72
Jakub Noga <noga.jakub@gmail.com>
2019-02-03 02:37:25 +0100

    Initialized server app

---
+++ homesensorscloud/src/main/java/io/softchameleon/homesensorscloud/HomeSensorsCloud.java
@@ -0,0 +1,14 @@
+package io.softchameleon.homesensorscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSensorsCloud {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSensorsCloud.class, args);
+	}
+
+}
+
