8a9ff72b791c5c5636ff131f9bc470e89ba2b511
Jakub Noga <noga.jakub@gmail.com>
2019-03-02 22:18:22 +0100

    Changed title and desc in pom

--- homesensorscloud/src/main/java/io/softchameleon/homesensorscloud/security/CustomUserDetailsService.java
+++ homesensorscloud/src/main/java/io/softchameleon/homesensorscloud/security/CustomUserDetailsService.java
@@ -7,10 +7,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 /**
  * CustomUserDetailsService
  */
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
    @Autowired
    PasswordEncoder encoder;
